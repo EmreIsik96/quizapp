88c43f675f730f2a02ba4b494244aa0bfe1eb8c2
Emre Isik <emre-isik@web.de>
2024-07-16 20:49:10 +0200

    new functions Added

--- script.js
+++ script.js
@@ -43,6 +43,9 @@ let questions = [
 let rightCounter = 0; // damit können wir den Wert immer bei einer Bedingung um eins erhöhen.
 let currentStart = 0; //damit wir erst immer den Array auswählen, wird hier einfach eine variable mit dem Wert 0 gespeichert. 
 
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_WRONG = new Audio ('audio/wrong.mp3');
+
 function init() //sieht die Funktion, dass beim Laden der Webseite von hier gestartet wird wie ein Render. 
 {
     document.getElementById('allQuestions').innerHTML = questions.length;
@@ -51,29 +54,15 @@ function init() //sieht die Funktion, dass beim Laden der Webseite von hier gest
 
 function showQuestionAndAnswers()
 {
-    if (currentStart >= questions.length) 
-    {   //show End Screen
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none';
-        document.getElementById('allQuestionsEnd').innerHTML = questions.length;
-        document.getElementById('rightAnswered').innerHTML = rightCounter;
+    if (gameIsOver()) 
+    {   
+        showEndScreen();
     }
     else
-    {   //show question
-    
-    let percent = (currentStart +1) / questions.length;
-    percent = percent * 100;
-    document.getElementById('percentCounter').innerHTML = `${percent}%`;
-    document.getElementById('percentCounter').style.width = `${percent}%`;
-
-    let question = questions[currentStart]; // neue variable wird erstellt und dem wird dann erst unser ArrayName zugewiesen und dann die variable die den Wert 0 hat. Damit wird immer auf das erste Array zugegriffen. 
-    document.getElementById('currentQuestion').innerHTML = currentStart +1;
-    document.getElementById('questionID').innerHTML = question['question']; //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
-    document.getElementById('answer_1').innerHTML = question['answer_1'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
-    document.getElementById('answer_2').innerHTML = question['answer_2'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
-    document.getElementById('answer_3').innerHTML = question['answer_3'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
-    document.getElementById('answer_4').innerHTML = question['answer_4'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
-}
+    {   
+        updateProgressBar();
+        showNextQuestions();
+    }
 }
 
 function answer(selection)
@@ -84,14 +73,15 @@ function answer(selection)
 
     if (selectedQuestionNumber == question['right_answer'] ) // nachdem wir jetzt 2 Zahlen vergleichen können, wird das per IF abgefragt. in dem Fall das angeklickte Feld daraus der String und aus dem String die Nummer zum Schluss. Das wird mit dem aus array entnommene "right_Answer = 4" verglichen. 
         {
-            
             document.getElementById(selection).classList.add('bg-success');
             rightCounter ++;
+            AUDIO_SUCCESS.play();
     }else
         {
             document.getElementById(selection).classList.add('bg-danger');
             document.getElementById(idOfRightAnswer).classList.add('bg-success');
-    }
+            AUDIO_WRONG.play();
+        }
     document.getElementById('nextButton').disabled = false; // somit kann man ein Button erst aktivieren lassen, wenn z.b die Frage beantwortet wurde. 
 }
 
@@ -107,18 +97,51 @@ function resetAnswerButtons()
 {
     document.getElementById('answer_1').classList.remove('bg-danger');
     document.getElementById('answer_1').classList.remove('bg-success');
-
     document.getElementById('answer_2').classList.remove('bg-danger');
     document.getElementById('answer_2').classList.remove('bg-success');
-
     document.getElementById('answer_3').classList.remove('bg-danger');
     document.getElementById('answer_3').classList.remove('bg-success');
-
     document.getElementById('answer_4').classList.remove('bg-danger');
     document.getElementById('answer_4').classList.remove('bg-success');
 }
 
 function restartGame()
 {
-    
+    rightCounter = 0; 
+    currentStart = 0;
+    document.getElementById('questionBody').style = "width: 500px; height: 550px; 'display: ' ";
+    document.getElementById('endScreen').style = 'display: none';
+    init();
+}
+
+function gameIsOver()
+{
+    return currentStart >= questions.length;
+}
+
+function showEndScreen()
+{
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('allQuestionsEnd').innerHTML = questions.length;
+    document.getElementById('rightAnswered').innerHTML = rightCounter;
+}
+
+function showNextQuestions()
+{
+    let question = questions[currentStart]; // neue variable wird erstellt und dem wird dann erst unser ArrayName zugewiesen und dann die variable die den Wert 0 hat. Damit wird immer auf das erste Array zugegriffen. 
+    document.getElementById('currentQuestion').innerHTML = currentStart +1;
+    document.getElementById('questionID').innerHTML = question['question']; //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
+    document.getElementById('answer_1').innerHTML = question['answer_1'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
+    document.getElementById('answer_2').innerHTML = question['answer_2'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
+    document.getElementById('answer_3').innerHTML = question['answer_3'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
+    document.getElementById('answer_4').innerHTML = question['answer_4'];  //hier wird einmal der name vom Array und dann das Objekt innere ausgewählt. 
+}
+
+function updateProgressBar()
+{
+    let percent = (currentStart +1) / questions.length;
+    percent = percent * 100;
+    document.getElementById('percentCounter').innerHTML = `${percent}%`;
+    document.getElementById('percentCounter').style.width = `${percent}%`;
 }
